3887e7c2214955ba2aae6eb884d8644bd08bc221
unknown <anuroopmishra21@gmail.com>
2019-03-23 15:21:51 +0530

    <my first application>

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    'use strict';
+    paper.install(window);
+    paper.setup(document.getElementById('mainCanvas'));
+    var c = Shape.Circle(200, 200, 80);
+    c.fillColor='black';
+    var text = new PointText(200, 200);
+    text.justification = 'center';
+    text.fillColor = 'white';
+    text.fontSize = 20;
+    text.content=' Hello World';
+    paper.view.draw();
+});
